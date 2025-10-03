d85dad20bb8425e2ae4abd9f19950dabf70b411d
João Mendes <joaoluiizmendes@gmail.com>
2025-10-03 17:14:10 -0300

    finalizando tela de login

--- devlog.md
+++ devlog.md
@@ -4,4 +4,10 @@ Iniciando projeto no dia 03/10/2025
 - Instalação do Yarn. 
 - Organizando Projeto. 
 - ESlint + Prettier no React.
-- Estilos Globais com Styled-Components
+- organizei os estilos Globais com Styled-Components.
+- Fiz a estrutura HTML da tela de login com react.
+- Estilizei a tela de Login.
+- Criando componentes para estilização dos botões.
+- Formulário com React Hook Form.
+- Validando o formulário de login.
+- Finalizando a tela de login.
---
+++ src/containers/Login/styles.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+import BackgroundLogin from '../../assets/background-login.svg';
+import Background from '../../assets/background.svg';
+
+export const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+`;
+
+export const LeftContainer = styled.div`
+    background: url('${BackgroundLogin}');
+    background-size: cover; //estica a imagem para caber na tela inteira
+    background-position: center;
+
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    img {
+        width: 80%;
+    }
+`;
+
+export const RightContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    height: 100%;
+    width: 100%;
+    max-width: 50%;
+
+    background: url('${Background}');
+    background-size: cover;
+    background-color: #1E1E1E;
+
+    p {
+        color: #FFF;
+        font-size: 18px;
+        font-weight: 800;
+
+        a {
+            text-decoration: underline;
+        }
+    }
+`;
+
+export const Title = styled.h2`
+    font-family: "Road Rage", sans-serif;
+    font-size: 40px;
+    color: #FFF;
+
+    span {
+        color: #9758A6;
+        font-family: "Road Rage", sans-serif;
+        justify-content: center;
+    }
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding: 20px;
+    width: 100%;
+    max-width: 400px;
+`;
+
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    width: 100%;
+
+    input {
+        width: 100%;
+        border: none;
+        height: 52px;
+        border-radius: 5px;
+        padding: 0 16px;
+    }
+
+    label {
+            font-size: 18px;
+            font-weight: 600;
+            color: #FFF;
+    }
+
+    p {
+        font-size: 14px;
+        line-height: 80%;
+        color: #CF3057;
+        font-weight: 600;
+        height: 10px;
+    }
+`;
